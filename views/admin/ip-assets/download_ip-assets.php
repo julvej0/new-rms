a0bbf1e8ea9becf9e8335ea7bfc75fd1192a5125
NorielleSilang <norielle.silang@g.batstate-u.edu.ph>
2023-04-29 17:18:21 +0800

    .

--- views/admin/ip-assets/download_ip-assets.php
+++ views/admin/ip-assets/download_ip-assets.php
@@ -1,4 +1,12 @@
 
+
+<?php 
+    include '../../../db/db.php';
+    require_once('functionalities/ipa-get-info-download.php');
+    $additionalQuery= authorSearch($conn);
+    $table_rows = get_data($conn, $additionalQuery);
+?>
+
 <link rel="stylesheet" href="./ip-assets.css">
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.0/jspdf.umd.min.js"></script>
@@ -34,52 +42,71 @@
 </style>
 
 <body>
+
     <main>
     <button onclick="downloadExcelFile()" class="btn" style="cursor: pointer;">Excel</button>
     <button onclick="downloadTableAsPDF()" class="btn" style="cursor: pointer;">PDF</button>
     <button onclick="exportTableToWord()" class="btn" style="cursor: pointer;">Word</button>
-        <div class="header">
-            <div class="left">
-                <form action="#">
-                    <div class="form-group">
-                        <input type="text" placeholder="Search...">
-                        <i class='bx bx-search icon' ></i>
-                    </div>
-                </form>
-            </div>
+    <form action='' method='get'>
+        <div class="form-group">
+            <input type='text' placeholder="Search" name='search' value='<?php $search_query?>' placeholder="Search..." >
+            <i class='bx bx-search icon' ></i>
         </div>
-        <section>
-            <div class="table-container">
-                <table>
+    </form>
+    <section>
+                <table id="mytable">
                     <thead>
                         <tr>
-                            <th>Registration Number</th>
-                            <th>Title</th>
-                            <th>Type</th>
-                            <th>Class of Work</th>
-                            <th>Date Registered</th>
-                            <th>Campus</th>
-                            <th>College</th>
-                            <th>Programs</th>
-                            <th>Authors</th>
+                            <th class="col-registration">Registration Number</th>
+                            <th class="col-title" style="min-width: 350px;">Title</th>
+                            <th class="col-type">Type</th>
+                            <th class="col-cow">Class of Work</th>
+                            <th class="col-date-cre">Date of Creation</th>
+                            <th class="col-date-reg">Date Registered</th>
+                            <th class="col-campus">Campus</th>
+                            <th class="col-college">College</th>
+                            <th class="col-program">Program</th>
+                            <th class="col-authors">Authors</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td style="min-width: 320px;">The Design and Development of A's Glass and Aluminum Online Booking System</td>
-                            <td>Type</td>
-                            <td>ClassOfWork</td>
-                            <td>January 1, 2023</td>
-                            <td>Campus</td>
-                            <td>College</td>
-                            <td>Programs</td>
-                            <td>Authors</td>
-                        </tr>
+                    <?php
+
+                         $search_query = isset($_GET['search']) ? $_GET['search'] : '';
+                       $result_count = pg_query($conn, "SELECT * FROM table_ipassets WHERE CONCAT(registration_number, title_of_work, type_of_document, class_of_work, date_of_creation, campus, college, program, authors) ILIKE '%$search_query%'".$additionalQuery.";");
+                       $total_records = pg_fetch_result($result_count, 0, 0);
+                       
+                       
+                       if ($table_rows !== null) {
+                        foreach ($table_rows as $row) {
+                    ?>
+                    <tr>
+                       
+                        <td class="reg-num-col col-registration"><?=$row['registration_number'];?></td>
+                        <td class="title-col col-title"><?=$row['title_of_work'];?></td>
+                        <td class="type-col col-type"><?=$row['type_of_document'];?></td>
+                        <td class="cow-col col-cow"><?=$row['class_of_work'];?></td>
+                        <td class="tbl-col"><?=$row['date_of_creation'];?></td>
+                        <td class="date-reg-col col-date-reg"><?=$row['date_registered'];?></td>
+                        <td class="campus-col col-campus"><?=$row['campus'];?></td>
+                        <td class="college-col col-college"><?=$row['college'];?></td>
+                        <td class="program-col col-program"><?=$row['program'];?></td>
+                        <td class="authors-col col-authors"><?=$row['authors'];?></td>
+
+                    </tr>
+                    <?php
+                    }
+                }else{
+                    ?>
+                    <tr>
+                        <td colspan='10' style="text-align:center">No Records Found!</td>
+                    </tr>
+                <?php
+                }
+                ?>
                     </tbody>
-                </table>
-            </div>
-        </section>
+            </table>
+    </section>
     </main>
 </section>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
@@ -92,7 +119,7 @@
     // Function to download the excel file
     function downloadExcelFile() {
         // Get the table element by its ID
-        var table = document.getElementById("invoice");
+        var table = document.getElementById("mytable");
 
         // Convert the table to a worksheet object
         var worksheet = XLSX.utils.table_to_sheet(table);
@@ -118,8 +145,36 @@
 
         // Trigger download
         var blob = new Blob([s2ab(excelData)], { type: "application/octet-stream" });
-        saveAs(blob, "author_list.xlsx");
+        saveAs(blob, "IPASSETS_list.xlsx");
     }
+
+  // download as pdf
+  function downloadTableAsPDF() {
+            var table = document.getElementById('mytable');
+            
+            html2pdf()
+                .from(table)
+                .save('table_example.pdf');
+        }
+
+// download as word document
+function exportTableToWord() {
+  var table = document.getElementById('mytable');
+  var html = table.outerHTML;
+
+  // Set page orientation and size
+  var style = "<style>@media print { @page { size: landscape; } }</style>";
+  html = style + html;
+
+  var blob = new Blob(['\ufeff', html], { type: 'application/vnd.ms-word;charset=utf-8' });
+  var url = URL.createObjectURL(blob);
+  var link = document.createElement('a');
+  link.href = url;
+  link.download = 'table.doc';
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
 
 </script>
 <?php
