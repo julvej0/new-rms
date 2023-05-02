03aa75c1d6628a663e7485b5b33519379d26aedc
gitcommit222 <TiomarOne02@gmail.com>
2023-05-02 11:38:55 +0800

    user-navbar

--- includes/public-user/templates/user-navbar.php
+++ includes/public-user/templates/user-navbar.php
@@ -1,6 +1,7 @@
 
 
 <!-- css -->
+<link rel="stylesheet" href="../../../css/index.css">
 <link rel="stylesheet" href="../../../includes/public-user/templates/user-navbar.css">
 
 <section>
@@ -8,14 +9,29 @@
         <div class="left">
             <div class="left-md-container">
                 <div class="left-sm-container">
-                    <p></p>
+                    <div class="left-content">
+                        <div class="logo">
+                            <img src="../../../assets/images/batStateUNeu-logo.png" alt="">
+                        </div>
+                        <div class="header-title">
+                            <h4>BatStateU</h4>
+                            <p>The National Engineering University</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
         <div class="right">
             <div class="right-md-container">
                 <div class="right-sm-container">
-                    <div class="content">
+                    <div class="right-content">
+                        <ul class="nav-links">
+                            <li><a href="#">HOME</a></li>
+                            <li><a href="#">PUBLICATIONS</a></li>
+                            <li><a href="#">IP ASSETS</a></li>
+                            <li><a href="#">ABOUT</a></li>
+                        </ul>
+                        <a href="#">SIGN IN</a>
                     </div>
                 </div>
             </div>
