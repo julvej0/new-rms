9309ed5aca7bf899bf8e913c7a87601bc3cb6425
gitcommit222 <TiomarOne02@gmail.com>
2023-05-03 11:09:27 +0800

    author filter-btn

--- views/admin/authors/authors.js
+++ views/admin/authors/authors.js
@@ -78,5 +78,40 @@ function rotateButton() {
 }
 
 
+// FILTER
+const allFilter = document.querySelectorAll('main .header .left .filter');
+allFilter.forEach(item => {
+    const filterBtn = item.querySelector('.btn');
+    const filterLink = item.querySelector('.filter-link');
+    const dropdownItems = item.querySelectorAll('a');
+
+    filterBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        filterLink.classList.toggle('show');
+    })
+
+    dropdownItems.forEach(choices => {
+        choices.addEventListener('click', () => {
+            const selected = choices.textContent;
+            filterBtn.innerHTML = selected;
+        });
+    });
+});
+
+window.addEventListener('click', (e) => {
+    allFilter.forEach(item => {
+        const filterBtn = item.querySelector('.btn');
+        const filterLink = item.querySelector('.filter-link');
+
+        if(e.target !== filterBtn){
+            if(e.target !== filterLink){
+                if(filterLink.classList.contains('show')){
+                    filterLink.classList.remove('show');
+                }
+            }
+        }
+    })
+})
+
 
   
--- views/admin/dashboard/dashboard.php
+++ views/admin/dashboard/dashboard.php
@@ -1,6 +1,5 @@
 <title>RMS | DASHBOARD</title>
 <?php 
-    include '../../../includes/loading.php';
     include '../../../includes/admin/templates/header.php';
     require_once "../../../db/db.php";
     include_once './functionalities/dashboard-function.php'; 
