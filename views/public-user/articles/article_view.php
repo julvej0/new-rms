5037c6fcc0782ec96e0c10c087a6f0aaf22eb92e
Neal-A <nealandrus.mancilla@g.batstate-u.edu.ph>
2023-05-09 11:56:11 +0800

    encrypt decrypt modification

--- views/public-user/articles/article_view.php
+++ views/public-user/articles/article_view.php
@@ -4,15 +4,14 @@ require_once "functionalities/config.php";
 
 if (isset($_GET['pubID']) && !empty($_GET['pubID'])) {
     $pubID = $_GET['pubID'];
-    $decrypted_ID = encryptor('decrypt', $pubID);
-    
-    $sql_data = "SELECT * FROM table_publications WHERE publication_id = $1;";
-    $params = array($decrypted_ID);
-    $sql_result = pg_query_params($conn, $sql_data, $params);
+    $decrypted_ID = encryptor('decrypt',$pubID);
 
-    while ($row = pg_fetch_assoc($sql_result)) {
+    $sql_data = "SELECT * FROM table_publications WHERE publication_id = $1;";
+    $stmt = pg_prepare($conn, "get_publication_by_id", $sql_data);
+    $result = pg_execute($conn, "get_publication_by_id", array($decrypted_ID));
+
+    while ($row = pg_fetch_assoc($result)) {
         echo $row['title_of_paper'];
     }
 }
-
 ?>
