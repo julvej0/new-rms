410b2e0da28dd0f066ac87a2bee5a2ea965c1d6f
Shrinkee <ralphivan.culla@g.batstate-u.edu.ph>
2023-05-10 09:46:33 +0800

    fixed user login and admin login

--- views/admin/account-management/functionalities/user-session.php
+++ views/admin/account-management/functionalities/user-session.php
@@ -2,31 +2,13 @@
 session_start();
 include_once "../../../db/db.php";
 
-// check if user is logged in
-if (!isset($_SESSION['user_email'])) {
-    header("Location: ../../admin/account/login.php");
-    session_destroy();
-    exit();
+// Check if the user is logged in as an admin
+if ($_SESSION['account_type'] != 'admin') {
+    header("Location: ../../../views/public-user/home/home.php");
+    exit;
 }
 
-// check if user has admin access
-if (!isset($_SESSION['user_role']) || $_SESSION['user_role'] !== 'admin') {
-    header("Location: ../../admin/account/login.php");
-    session_destroy();
-    exit();
-}
-
-// check if accessed through valid entry point
-$referrer = $_SERVER['HTTP_REFERER'];
-$valid_entry_point = "http://localhost/new-rms-webdev/views/admin/account/login.php"; // replace with your actual login page URL
-
-if ($referrer !== $valid_entry_point) {
-    header("Location: ../../admin/account/login.php");
-    session_destroy();
-    exit();
-}
-
-// fetch user details from database
+// Fetch user details from the database
 $user_query = "SELECT * FROM table_user WHERE email = $1";
 $user_result = pg_query_params($conn, $user_query, array($_SESSION['user_email']));
 if (!$user_result) {
