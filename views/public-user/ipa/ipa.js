7c167ec9c3bfa46766120c1ce5b6a7ffe006a962
gitcommit222 <TiomarOne02@gmail.com>
2023-05-18 15:11:32 +0800

    sort and filter btn

---
+++ views/public-user/ipa/ipa.js
@@ -0,0 +1,32 @@
+const sortBtn = document.getElementById('btn-sort');
+const sortOptions = document.querySelector('#main-content .header .sort-btn .sort-links');
+
+
+sortBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    sortOptions.classList.toggle('show');
+})
+
+const filterBtn = document.getElementById('btn-filter');
+const filterOptions = document.querySelector('.filter-options');
+const closeBtn = document.querySelector('#main-content .header .filter-btn .filter-options form .icon');
+
+filterBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    filterOptions.classList.add('show');
+})
+
+window.addEventListener('click', (e) => {
+    if(e.target !== sortBtn){
+        if(e.target !== sortOptions) {
+            if(sortOptions.classList.contains('show')){
+                sortOptions.classList.remove('show');
+            }
+        }
+    }
+
+    if(e.target === closeBtn) {
+        filterOptions.classList.remove('show')
+    }
+    
+})
