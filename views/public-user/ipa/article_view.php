a0c55c9bb62778a0a1dc780206ba2dba779a9241
Shrinkee <ralphivan.culla@g.batstate-u.edu.ph>
2023-05-21 10:08:00 +0800

    .

---
+++ views/public-user/ipa/article_view.php
@@ -0,0 +1,26 @@
+<link rel="stylesheet" href="../../../views/public-user/articles/article-view.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+<?php 
+    include '../../../includes/admin/templates/header.php';
+    include '../../../includes/public-user/templates/user-navbar.php'; 
+    include '../../../db/db.php';
+    include_once "functionalities/articles-functions.php";
+?>
+
+<body>
+    <section id="main-content">
+        <div class="page-title">
+            <h3 class="animate__animated animate__zoomIn">PUBLICATIONS</h3>
+        </div>
+        <div class="content-container animate__animated animate__fadeInUp">
+            <?php
+                if (isset($_GET['pubID']) && !empty($_GET['pubID'])) {
+                    $row = getPublicationData($_GET['pubID'], $conn);
+                    if ($row) {
+                        displayPublicationData($row, $conn);
+                    }
+                }
+            ?>
+        </div>
+    </section>   
+</body>
