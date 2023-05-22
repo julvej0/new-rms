211b703b46c2903d63389eb0b6ffdf4726cad654
Shrinkee <ralphivan.culla@g.batstate-u.edu.ph>
2023-05-22 13:37:44 +0800

    added comments on login

--- views/admin/account/functionalities/logout.php
+++ views/admin/account/functionalities/logout.php
@@ -1,5 +1,7 @@
 <?php
+//this is for the the destroying the session of the user, when he/she logged out.
     session_start();
+    //if he/she is an admin it will go to login page, else it wiould go to the home page which is for the public-users.
     if($_SESSION['account_type'] == 'admin'){
         session_destroy();
         header('Location: ../login.php');
