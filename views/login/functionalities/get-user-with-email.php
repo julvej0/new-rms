64902731ec96cc1efbd84b3c070acea6b6d696b5
John Michael Julve <julvej0@gmail.com>
2024-02-23 15:35:27 +0800

    get user with email to api

--- views/login/functionalities/get-user-with-email.php
+++ views/login/functionalities/get-user-with-email.php
@@ -5,17 +5,36 @@ if (!isset($_POST['user_email'])) {
     return;
 }
 
-$fetch_query = "SELECT * FROM table_users WHERE email = $1";
-$fetch_result = pg_query_params($conn, $fetch_query, array($_POST['user_email']));
-if (!$fetch_result) {
-    echo ("userWithEmailQueryFailed err: " . pg_last_error($conn));
-    exit;
+$response = file_get_contents($userurl);
+
+if ($response === false) {
+    echo "An error occurred while fetching user data.";
+    exit();
 }
 
 $user = null;
-if (pg_num_rows($fetch_result) > 0) {
-    $user = pg_fetch_assoc($fetch_result);
+$userData = json_decode($response, true)['table_user'];
+foreach ($user as $user) {
+    $userId = $user['user_id'];
+    if ($user['email'] === $email) {
+        return $userId;
+    }
 }
-
+if(!$userData){
+    $user = $userData;
+}
 echo json_encode(array("user" => $user));
+// $fetch_query = "SELECT * FROM table_users WHERE email = $1";
+// $fetch_result = pg_query_params($conn, $fetch_query, array($_POST['user_email']));
+// if (!$fetch_result) {
+//     echo ("userWithEmailQueryFailed err: " . pg_last_error($conn));
+//     exit;
+// }
+
+// $user = null;
+// if (pg_num_rows($fetch_result) > 0) {
+//     $user = pg_fetch_assoc($fetch_result);
+// }
+
+// echo json_encode(array("user" => $user));
 ?>
