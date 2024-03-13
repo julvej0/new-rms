02b16a5eb1718a35faa24deb03190aa2b1dd6e7d
srrscx <114233971+srrscx@users.noreply.github.com>
2024-03-13 11:51:10 +0800

    minor changes

--- views/admin/ip-assets/edit-ipa.php
+++ views/admin/ip-assets/edit-ipa.php
@@ -70,7 +70,7 @@ if (isset($_POST['edit'])) {
                                                 <option value="<?= $row['campus'] ?>" hidden>
                                                     <?= $row['campus'] ?>
                                                 </option>
-                                                <option value="Alangilan">Alangilan</option>
+                                                <option value="Alangilan (Main II)">Alangilan (Main II)</option>
                                                 <option value="Balayan">Balayan</option>
                                                 <option value="Lemery">Lemery</option>
                                                 <option value="Lipa">Lipa</option>
@@ -78,7 +78,7 @@ if (isset($_POST['edit'])) {
                                                 <option value="Mabini">Mabini</option>
                                                 <option value="Malvar">Malvar-JPCPC</option>
                                                 <option value="Nasugbu">Nasugbu-Arasof</option>
-                                                <option value="Pablo Borbon (Main I)">Pablo Borbon</option>
+                                                <option value="Pablo Borbon (Main I)">Pablo Borbon (Main I)</option>
                                                 <option value="Padre Garcia">Padre Garcia</option>
                                                 <option value="Rosario">Rosario</option>
                                                 <option value="San Juan">San Juan</option>
