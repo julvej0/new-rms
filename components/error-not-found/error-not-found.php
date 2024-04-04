3e247ed67e83e3a09dd81127262d12c6b01a5276
srrscx <cxyreussadaya@gmail.com>
2024-04-04 14:50:25 +0800

    minor changes

---
+++ components/error-not-found/error-not-found.php
@@ -0,0 +1,23 @@
+<title>Page Not Found</title>
+<?php
+include dirname(__FILE__, 2) . '/header/header.php';
+?>
+
+<link rel="stylesheet" href="./error.css">
+
+<body>
+    <div id="loading-screen">
+    </div>
+    <section>
+        <main>
+            <div class="container">
+                <div class="logo" >
+                    <img src="../../assets/images/redspartan_logo.png" alt="redSpartan">
+                </div>
+                <h1>404 ERROR</h1>
+                <p class="p1" >This page doesn't exist.</p>
+                <p class="p2" >Would you like to <a href="../../views/admin/publications/publications.php" >go back</a>?</p>
+            </div>
+        </main>
+    </section>
+</body>
